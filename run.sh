1772605cd7152e4b5f8f7b7f4eb552bfbb455749
Ricardo Gil Alcañiz <rgil@okode.com>
2017-07-12 12:12:44 +0200

    Adding new user support with manager-script role

--- run.sh
+++ run.sh
@@ -7,6 +7,14 @@ if [[ ${MANAGER_PASSWORD} ]]; then
     echo "Manager password configuration detected, setting it"
     sed -i -e "s/password=\"admin\"/password=\"$MANAGER_PASSWORD\"/g" /opt/tomcat7/conf/tomcat-users.xml
 fi
+if [[ ${SCRIPT_USER} ]]; then
+    echo "Script username configuration detected, setting it to $SCRIPT_USER"
+    sed -i -e "s/username=\"scriptuser\"/username=\"$SCRIPT_USER\"/g" /opt/tomcat7/conf/tomcat-users.xml
+fi
+if [[ ${SCRIPT_PASSWORD} ]]; then
+    echo "Script user password configuration detected, setting it"
+    sed -i -e "s/password=\"scriptpass\"/password=\"$SCRIPT_PASSWORD\"/g" /opt/tomcat7/conf/tomcat-users.xml
+fi
 if [[ ${CONTEXT_CONFIG} ]]; then
     echo "Context file configuration detected, setting it"
     sed -i -e "s#</Context>#$CONTEXT_CONFIG</Context>#g" /opt/tomcat7/conf/context.xml
